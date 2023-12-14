5df77c4d5a681ba23fe8ab333ba500e20e85cc1b
helalkhandev <helal.khan2013@gmail.com>
2023-12-15 01:12:53 +0600

    Card

---
+++ client/src/pages/Home/Card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="flex justify-center">
+      <div className="border p-5 m-5">Card-1</div>
+      <div className="border p-5 m-5">Card-2</div>
+      <div className="border p-5 m-5">Card-3</div>
+    </div>
+  );
+}
+
+export default Card;
